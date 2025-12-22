40c9341d6befb4eb95909c32a2ca782c073d4221
unknown <turing175@gmail.com>
2025-12-22 13:42:50 +0800

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: 'class',
   theme: {
